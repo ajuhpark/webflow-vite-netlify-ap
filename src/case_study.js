f1824d14df9ade57a8979e996724a9e354c5888f
ajuhpark <ajuhpark@gmail.com>
2024-04-23 20:11:56 -0700

    added onLoad animations for homepage and first case study.

---
+++ src/case_study.js
@@ -0,0 +1,22 @@
+import './styles/style.css'
+
+import { gsap } from 'gsap'
+
+import feature from './features/feature'
+
+console.log('Welcome to Vite + JS + Webflow!')
+// console.log(gsap)
+
+// alert('Local')
+
+feature()
+
+// Home - Load Animation - Case Studies
+var case_study_tl = gsap.timeline({ defaults: { ease: 'power1.inOut'} })
+case_study_tl
+  .from('.proj', { backgroundColor: '#f7f7f2', opacity: 0, duration: 1.2, autoAlpha: 0}, 'case_study_tl')
+  .from('.project_title', { yPercent: 60, duration: .9 }, 'case_study_tl+=.4')
+  .from('.project_title', { opacity: 0, duration: .7 }, 'case_study_tl+=.4')
+  .from('.project_hero_image', { yPercent: 20, duration: .8 }, 'case_study_tl+=.5')
+  .from('.project_hero_image', { opacity: 0, duration: .9 }, 'case_study_tl+=.6')
+  .from('.project_body_section', { opacity: 0, duration: 1.2 }, 'case_study_tl+=.6')
--- src/main.js
+++ src/main.js
@@ -5,8 +5,25 @@ import { gsap } from 'gsap'
 import feature from './features/feature'
 
 console.log('Welcome to Vite + JS + Webflow!')
-console.log(gsap)
+// console.log(gsap)
 
 // alert('Local')
 
 feature()
+
+var home_tl = gsap.timeline({ defaults: { ease: 'power1.inOut'} })
+/*autoAlpha below is for the Flash of Unstyled Content issue.*/
+home_tl
+  .from('Body', { backgroundColor: '#f7dcdc', opacity: 0, duration: 1.2 }, 'home_start')
+  .from('.home_icon', { yPercent: 110, duration: 1 }, 'home_start+=.3')
+  .from('.home_icon', { opacity: 0, duration: .8 }, 'home_start+=.6')
+  .from('.home_icon_f1', { yPercent: 110, duration: 1 }, 'home_start+=.3')
+  .from('.home_icon_f1', { opacity: 0, duration: .8 }, 'home_start+=.6')
+  .from('.home_icon_f2', { yPercent: 110, duration: 1 }, 'home_start+=.3')
+  .from('.home_icon_f2', { opacity: 0, duration: .8 }, 'home_start+=.6')
+  .from('.home_name', { yPercent: 110, duration: .7 }, 'home_start+=.4' )
+  .from('.home_name', { opacity: 0, duration: .9 }, 'home_start+=.4' )
+  .from('.home_position', { yPercent: 110, duration: .7 }, 'home_start+=.4' )
+  .from('.home_position', { opacity: 0, duration: .9 }, 'home_start+=.4' )
+  .from('.cs_component_container_1', { yPercent: 10, duration: 1.2 }, 'home_start+=.2' )
+  .from('.cs_component_container_1', { opacity: 0, duration: 1.7 }, 'home_start+=.1' )
