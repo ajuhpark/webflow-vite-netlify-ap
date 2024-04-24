23b770cbbee95bfd5f66c927c99e1d102be621ef
ajuhpark <ajuhpark@gmail.com>
2024-04-23 22:53:15 -0700

    changes w the load interactions

--- src/case_study.js
+++ src/case_study.js
@@ -14,8 +14,9 @@ feature()
 // Home - Load Animation - Case Studies
 var case_study_tl = gsap.timeline({ defaults: { ease: 'power1.inOut'} })
 case_study_tl
-  .from("html", {opacity:0, duration:0, autoAlpha:0, backgroundColor: '#f7dcdc'}, 'case_study_tl')
-  .from('.proj', { backgroundColor: '#f7f7f2', opacity: 0, duration: 1.2}, '<')
+  //#f7f7f2 is light beige. #f7dcdc is light pink.
+  .from("html", {opacity:0, duration: 1, autoAlpha: 0, backgroundColor: '#f7f7f2'}, 'case_study_tl')
+  .from('.proj', { backgroundColor: '#f7f7f2', opacity: 0, duration: 0.8}, '<')
   .from('.project_title', { yPercent: 60, duration: .9 }, 'case_study_tl+=.4')
   .from('.project_title', { opacity: 0, duration: .7 }, 'case_study_tl+=.4')
   .from('.project_hero_image', { yPercent: 20, duration: .8 }, 'case_study_tl+=.5')
--- src/main.js
+++ src/main.js
@@ -14,8 +14,9 @@ feature()
 var home_tl = gsap.timeline({ defaults: { ease: 'power1.inOut'} })
 /*autoAlpha below is for the Flash of Unstyled Content issue.*/
 home_tl
-  .from("html", {opacity:0, duration:0, autoAlpha:0}, 'home_start')
-  .from('Body', { backgroundColor: '#f7dcdc', opacity: 0, duration: 1.2 }, '<')
+  .from("html", {opacity:0, backgroundColor: '#f7dcdc', duration: 0.6, autoAlpha:0}, 'home_start')
+  // #f7dcdc is pink
+  .from('Body', { backgroundColor: '#f7dcdc', opacity: 0, duration: 0.2 }, '<')
   .from('.home_icon', { yPercent: 110, duration: 1 }, 'home_start+=.3')
   .from('.home_icon', { opacity: 0, duration: .8 }, 'home_start+=.6')
   .from('.home_icon_f1', { yPercent: 110, duration: 1 }, 'home_start+=.3')
