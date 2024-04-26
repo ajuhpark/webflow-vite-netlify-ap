f2c8673b3ec00383e7a5d395c88392814f6e91d3
ajuhpark <ajuhpark@gmail.com>
2024-04-25 23:04:14 -0700

    right now only the case_study.js works. i'm trying to comment out the library from package.json.

--- 2.vite.config.js
+++ 2.vite.config.js
@@ -5,11 +5,13 @@ const { defineConfig } = require('vite')
 module.exports = defineConfig({
   build: {
     // this building a library is from this website: https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
+    /*
     lib: {
       entry: path.resolve(__dirname, 'lib/main.js'),
       name: 'webflow_js_library',
       fileName: (format) => `webflow_js_library.${format}.js`
     },
+    */
     // I put this back from the original webflow + vite.js file.
     rollupOptions: {
       input: 'lib/case_study.js',
