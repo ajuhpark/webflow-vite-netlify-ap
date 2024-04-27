43f5ddbfe2b1a011cc600d848b1a4045311a2bc4
ajuhpark <ajuhpark@gmail.com>
2024-04-27 10:02:56 -0700

    adding a js file for the website builder tool page. also configured package.json and vite.config files.

---
+++ 3.vite.config.js
@@ -0,0 +1,63 @@
+// 2.vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    // this building a library is from this website: https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
+    /*
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'webflow_js_library',
+      fileName: (format) => `webflow_js_library.${format}.js`
+    },
+    */
+    // I put this back from the original webflow + vite.js file.
+    rollupOptions: {
+      input: 'lib/cs_website_builder_tools.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'cs_website_builder_tools.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  }
+});
+
+/* 
+// original vite.config.js code below
+// vite.config.js
+export default defineConfig({
+  plugins: [eslintPlugin({ cache: false })],
+  server: {
+    host: 'localhost',
+    cors: '*',
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  },
+})
+*/
